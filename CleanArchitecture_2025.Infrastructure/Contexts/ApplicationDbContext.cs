041d24109f825e6cd9ce4db38cdf03d1afb4abfb
ahguney <ahmetguney090@gmail.com>
2025-10-21 18:21:32 +0300

    feat(infrastructure): implement data access layer
    - Add EF Core DbContext
    - Implement employee repository
    - Configure infrastructure services
    - Add required NuGet packages

---
+++ CleanArchitecture_2025.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture_2025.Domain.Employees;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture_2025.Infrastructure.Contexts
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> employees { get; set; }
+    }
+}
---
+++ CleanArchitecture_2025.Infrastructure/InfrastructureRegistrar.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchitecture_2025.Domain.Employees;
+using CleanArchitecture_2025.Infrastructure.Contexts;
+using CleanArchitecture_2025.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture_2025.Infrastructure
+{
+    public static class InfrastructureRegistrar
+    {
+        public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt => {
+                string connectionString = configuration.GetConnectionString("SqlServer")!;
+                opt.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IEmployeeRepository,EmployeeRepository>();
+
+            services.Scan(opt => opt
+            .FromAssemblies(typeof(InfrastructureRegistrar).Assembly)
+            .AddClasses(publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+       );
+
+
+            return services;
+        
+        }
+    }
+}
